a4c4f9807610e17bf639b4739a11085df5c35cee
Arovan393 <vadimmeasnikov@gmail.com>
2023-09-23 15:39:36 +0300

    add title

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './style/App.css';
 function App() {
   return (
     <div className="App">
+      <h1>React</h1>
       <div class="haeder">
         <div class="haeder-container">
           <div class="navigation">
